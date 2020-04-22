cb93a623755d151c35d9f3c1930c35be0116560b
JackSnowdon <jacksnowdondrums@gmail.com>
2020-04-22 18:01:57 +0100

    UX updates and updated Base model to define good or evil

--- world/models.py
+++ world/models.py
@@ -4,6 +4,13 @@ from accounts.models import Profile
 
 # Create your models here.
 
+Player = "Player"
+Enemy = "Enemy"
+
+ALIGNCHOICES = (
+        (Player, "Player"),
+        (Enemy, "Enemy"),
+    )
 
 class Base(models.Model):
     name = models.CharField(max_length=255)
@@ -15,6 +22,8 @@ class Base(models.Model):
     xp = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000)], default=1)
     level = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)], default=1)
     created_by = models.ForeignKey(Profile, related_name='heroes', on_delete=models.PROTECT)
+    alignment = models.CharField(
+        choices=ALIGNCHOICES, default=Player, max_length=100)
 
     def __str__(self):
         return self.name
